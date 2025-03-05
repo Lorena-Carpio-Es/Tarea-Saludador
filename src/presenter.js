ec92682b3987779ae01edb825b8da6ca79648b1b
Lorena-Carpio-Es <karylore1@gmail.com>
2025-03-04 22:30:36 -0400

    Saludar con nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,6 @@
 import saludar from "./saludador.js"; // Importar la función saludar
 
+const nombreInput = document.querySelector("#nombre"); // Obtener el campo de entrada del nombre
 const form = document.querySelector("#saludar-form"); // Obtener el formulario
 const div = document.querySelector("#resultado-div"); // Obtener el div para mostrar el resultado
 
@@ -7,6 +8,9 @@ const div = document.querySelector("#resultado-div"); // Obtener el div para mos
 form.addEventListener("submit", (event) => {
   event.preventDefault(); // Evitar que el formulario se envíe
 
-  // Llamar a la función saludar y mostrar el resultado en el div
-  div.innerHTML = "<p>" + saludar() + "</p>";
+  const nombre = nombreInput.value; // Obtener el valor del campo de entrada
+  const saludo = saludar(nombre); // Llamar a la función saludar con el nombre
+
+  // Mostrar el resultado en el div
+  div.innerHTML = "<p>" + saludo + "</p>";
 });
--- src/saludador.test.js
+++ src/saludador.test.js
@@ -1,9 +1,13 @@
-import saludar from "./salulador.js";
+import saludar from "./saludador.js";
 
 describe("Saludar", () => {
   it("deberia saludar con Hola", () => {
     expect(saludar()).toEqual("Hola");
   });
+  it("deberia saludar con 'Hola, Juan' si el nombre es Juan", () => {
+    expect(saludar("Juan")).toEqual("Hola, Juan");
+  });
+  
 });
 
 
